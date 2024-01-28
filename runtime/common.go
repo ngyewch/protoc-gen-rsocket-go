7121b5c4a402890d83a3a2b33e29d1be4cd7e87f
Nick Ng <ngyewch@gmail.com>
2024-01-28 15:19:30 +0800

    feat: async client handler

--- runtime/common.go
+++ runtime/common.go
@@ -41,6 +41,29 @@ func HandleClientRequestResponse(ctx context.Context, selector uint64, methodNam
 	return handler(ctx, reqWrapperBytes)
 }
 
+func HandleClientRequestResponseAsync(ctx context.Context, selector uint64, methodName string, req proto.Message, handler ClientRequestResponseHandlerAsync) *mo.Future[[]byte] {
+	reqBytes, err := proto.Marshal(req)
+	if err != nil {
+		return mo.NewFuture(func(resolve func([]byte), reject func(error)) {
+			reject(err)
+		})
+	}
+	reqWrapper := &RequestWrapper{
+		Selector:   selector,
+		MethodName: methodName,
+		Payload:    reqBytes,
+	}
+	outputBuffer := bytes.NewBuffer(nil)
+	err = reqWrapper.Marshal(outputBuffer)
+	if err != nil {
+		return mo.NewFuture(func(resolve func([]byte), reject func(error)) {
+			reject(err)
+		})
+	}
+	reqWrapperBytes := outputBuffer.Bytes()
+	return handler(ctx, reqWrapperBytes)
+}
+
 func HandleServerRequestResponse(ctx context.Context, reqWrapperBytes []byte, handler ServerRequestResponseHandler) ([]byte, error) {
 	inputBuffer := bytes.NewBuffer(reqWrapperBytes)
 	var reqWrapper RequestWrapper
